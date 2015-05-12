d4e060ebc5bdb99ec87b9921ed27fa6ce1d55759
Ahmet Abdi <ahmetabdi@gmail.com>
2015-05-12 16:25:36 +0100

    :boom:

--- lib/tmdb/movie.ex
+++ lib/tmdb/movie.ex
@@ -4,18 +4,18 @@ defmodule Tmdb.Movie do
   end
 
   def top_rated do
-    Tmdb.get("/movie/top_rated")
+    Tmdb.get("movie/top_rated")
   end
 
   def now_playing do
-    Tmdb.get("/movie/now_playing")
+    Tmdb.get("movie/now_playing")
   end
 
   def upcoming do
-    Tmdb.get("/movie/upcoming")
+    Tmdb.get("movie/upcoming")
   end
 
   def latest do
-    Tmdb.get("/movie/latest")
+    Tmdb.get("movie/latest")
   end
 end
